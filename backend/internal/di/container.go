354cd174b88714f7cd5d128e00e7d819df730de3
makarim muhammad <jakamakarim@gmail.com>
2026-04-07 15:41:36 +0700

    feat : initialize container

---
+++ backend/internal/di/container.go
@@ -0,0 +1,42 @@
+package di
+
+import (
+	"fmt"
+	"makarim22/shorten-your-link/internal/handlers"
+	"makarim22/shorten-your-link/internal/repository"
+	"makarim22/shorten-your-link/internal/service"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Container struct {
+	db *pgxpool.Pool
+
+	authHandler *handlers.AuthHandler
+	authService *service.AuthService
+	userRepo    *repository.UserRepository
+}
+
+func NewContainer(db *pgxpool.Pool) (*Container, error) {
+	if db == nil {
+		return nil, fmt.Errorf("database connection cannot be nil")
+	}
+
+	container := &Container{
+		db: db,
+	}
+
+	container.initDependencies()
+
+	return container, nil
+}
+
+func (c *Container) initDependencies() {
+	c.userRepo = repository.NewUserRepository(c.db)
+	c.authService = service.NewAuthService(c.userRepo)
+	c.authHandler = handlers.NewAuthHandler(c.authService)
+}
+
+func (c *Container) AuthHandler() *handlers.AuthHandler {
+	return c.authHandler
+}
