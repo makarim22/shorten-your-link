6ce7bdafd77f064f622b5394599a824070388542
makarim muhammad <jakamakarim@gmail.com>
2026-04-07 21:34:14 +0700

    feat: add shortenedUrl response

--- backend/internal/models/response.go
+++ backend/internal/models/response.go
@@ -22,13 +22,24 @@ type UserResponse struct {
 	CreatedAt time.Time `json:"created_at"`
 }
 
+//type LinkResponse struct {
+//	ID          int        `json:"id"`
+//	ShortCode   string     `json:"short_code"`
+//	OriginalUrl string     `json:"original_url"`
+//	IsCustom    bool       `json:"is_custom"`
+//	CreatedAt   time.Time  `json:"created_at"`
+//	ExpiresAt   *time.Time `json:"expires_at,omitempty"`
+//}
+
 type LinkResponse struct {
-	ID          int       `json:"id"`
-	ShortCode   string    `json:"short_code"`
-	OriginalUrl string    `json:"original_url"`
-	IsCustom    bool      `json:"is_custom"`
-	CreatedAt   time.Time `json:"created_at"`
-	ExpiresAt   time.Time `json:"expires_at,omitempty"`
+	ID           int        `json:"id"`
+	UserID       int        `json:"user_id"`
+	OriginalUrl  string     `json:"original_url"`
+	ShortCode    string     `json:"short_code"`
+	ShortenedUrl string     `json:"shortened_url"`
+	IsCustom     bool       `json:"is_custom"`
+	CreatedAt    time.Time  `json:"created_at"`
+	ExpiresAt    *time.Time `json:"expires_at,omitempty"`
 }
 
 type AuthResponse struct {
