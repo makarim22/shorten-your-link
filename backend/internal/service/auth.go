267ecf7c12347183d1e0590c2e975acefe821073
makarim muhammad <jakamakarim@gmail.com>
2026-04-07 14:51:27 +0700

    feat :  create register service function

--- backend/internal/service/auth.go
+++ backend/internal/service/auth.go
@@ -3,6 +3,7 @@ package service
 import (
 	"context"
 	"fmt"
+	"makarim22/shorten-your-link/internal/lib"
 	"makarim22/shorten-your-link/internal/models"
 	"makarim22/shorten-your-link/internal/repository"
 )
@@ -17,10 +18,30 @@ func NewAuthService(repo *repository.UserRepository) *AuthService {
 	}
 }
 
-func (s *AuthService) Register(ctx context.Context, request *models.RegisterRequest) (*models.AuthResponse, error) {
+func (s *AuthService) Register(ctx context.Context, request *models.RegisterRequest) (*models.UserResponse, error) {
 	existingUser, err := s.repo.GetByEmail(ctx, request.Email)
-	if err != nil {
+	if err != nil && existingUser != nil {
 		return nil, fmt.Errorf("already registered email: %w", err)
 	}
+
+	hashedPass, err := lib.HashPassword(request.Password)
+	if err != nil {
+		return nil, fmt.Errorf("cannot hashing password: %w", err)
+	}
+
+	user := &models.User{
+		Email:        request.Email,
+		PasswordHash: hashedPass,
+	}
+
+	user, err = s.repo.Create(ctx, user)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create user: %w", err)
+	}
+	return &models.UserResponse{
+		ID:        user.ID,
+		Email:     user.Email,
+		CreatedAt: user.CreatedAt,
+	}, nil
 
 }
